9173624b99a49f82d791ef0a0506ba50d009dbcd
manjula doddavula <manjuladoddavula@gmail.com>
2025-04-16 11:35:39 +0530

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.calculatormvc"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.calculatormvc"
